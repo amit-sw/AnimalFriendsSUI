68b5a5a095996d311ad812c134ff2e65097e429d
Amit Gupta <amit@gprof.com>
2021-06-30 12:01:30 -0700

    Initial Commit

---
+++ AnimalFriendsSUI/AnimalFriendsSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalFriendsSUIApp.swift
+//  AnimalFriendsSUI
+//
+//  Created by Amit Gupta on 6/30/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimalFriendsSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimalFriendsSUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimalFriendsSUI
+//
+//  Created by Amit Gupta on 6/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
